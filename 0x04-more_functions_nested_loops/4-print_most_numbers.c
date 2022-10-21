d63062bb5175b643f3d9d1192ea59d3ac17298b7
Aanuoluwa101 <aanuayodeji101@gmail.com>
2022-10-21 02:54:41 -0700

    more functions corrections 1

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,16 +6,16 @@
 * Return:
 */
 
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	int c;
 
 	for (c = 0; c <= 9; c++)
 	{
-		if (c != 2 || c != 4)
-			_putchar('0' + c);
-		else
+		if (c == 2 || c == 4)
 			continue;
+		else
+			_putchar('0' + c);
 	}
 
 	_putchar('\n');
