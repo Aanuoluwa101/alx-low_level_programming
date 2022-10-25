bb82b6296bfe0c2ddf9b1f6ac9edc5cd1552e6f2
Aanuoluwa101 <aanuayodeji101@gmail.com>
2022-10-25 02:59:50 -0700

    print-array correction 2

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,13 +12,18 @@ void print_array(int *a, int n)
 {
 	int pos;
 
-	for (pos = 0; pos <= n; pos++)
+	if (n <= 0)
 	{
-		if (pos == n - 1)
-			printf("%d\n", *(a + pos));
-		else if (pos == n)
-			printf("%d\n", *(a + pos));
-		else
-			printf("%d, ", *(a + pos));
+		printf("\n");
+	}
+	else
+	{
+		for (pos = 0; pos < n; pos++)
+		{
+			if (pos == n - 1)
+				printf("%d\n", *(a + pos));
+			else
+				printf("%d, ", *(a + pos));
+		}
 	}
 }
