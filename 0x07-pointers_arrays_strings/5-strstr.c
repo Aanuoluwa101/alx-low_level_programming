73eb469142bfd7eb647e369428b2e95af21538c9
Aanuoluwa101 <aanuayodeji101@gmail.com>
2022-10-31 22:07:25 -0700

    even more pointers: task 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,58 @@
+#include "main.h"
+
+/**
+* _strstr - locates a substring
+* @haystack: the string to be searched
+* @needle: the substring
+* Return: a pointer to the beginning of the located substring,
+* or NULL if the substring is not found.
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+	int j;
+	int k;
+	int s_len;
+	int n_len;
+	char n;
+
+	s_len = _strlen(haystack);
+	n_len = _strlen(needle);
+	n = needle[0];
+
+	for (i = 0; haystack[i]; i++)
+	{
+		if (haystack[i] == n)
+		{
+			if (s_len - i >= n_len)
+			{
+				for (j = i, k = 0; k < s_len && j < n_len; j++, k++)
+					if (haystack[j] != needle[k])
+						break;
+			return (&haystack[i]);
+			}
+		}
+	}
+	return ('\0');
+}
+
+
+/**
+* _strlen - finds the length of a string
+* @s: the string in question
+* Return: the length of string s
+*/
+
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (*(s + count) != '\0')
+	{
+		count++;
+	}
+
+	return (count);
+}
+
