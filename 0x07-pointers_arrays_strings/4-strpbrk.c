1e593c3c3825a8fc759a00c972e7e2612eb65f9e
Aanuoluwa101 <aanuayodeji101@gmail.com>
2022-10-31 21:28:46 -0700

    even more pointers: task 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,60 @@
+#include "main.h"
+
+/**
+* _strpbrk - searches a string for any of a set of bytes.
+* @s: the string to be searched
+* @accept: the string whose bytes ae to be searched for
+*
+* Return: a pointer to the byte in s that matches one of the
+* bytes in accept, or NULL if no such byte is found
+*/
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+	int j;
+	int index;
+	int found;
+
+	found = 0;
+	index = 0;
+
+	for (i = 0; i < _strlen(accept); i++)
+	{
+		for (j = 0; j < _strlen(s); j++)
+		{
+			if (s[j] == accept[i])
+			{
+				found++;
+				if (found == 1)
+					index = j;
+				else if (found > 1 && j < index)
+					index = j;
+			}
+		}
+	}
+	if (found > 0)
+		return (&s[index]);
+
+	return ('\0');
+}
+
+
+/**
+* _strlen - finds the length of a string
+* @s: the string in question
+* Return: the length of string s
+*/
+
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (*(s + count) != '\0')
+	{
+		count++;
+	}
+
+	return (count);
+}
+
