6951eda5df20b991dc86f601da02527b114fc6e2
Aanuoluwa101 <aanuayodeji101@gmail.com>
2022-11-08 20:59:42 -0800

    correcting alloc_grid again

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -24,7 +24,10 @@ int **alloc_grid(int width, int height)
 		{
 			ptr[i] = malloc(width * sizeof(int));
 			if (ptr[i] == NULL)
+			{
+				free(ptr);
 				return (NULL);
+			}
 		}
 		for (i = 0; i < height; i++)
 		{
