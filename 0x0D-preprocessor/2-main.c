126e2a0e70aba69c998106032b0f532a13ca2a32
Aanuoluwa101 <aanuayodeji101@gmail.com>
2022-11-14 02:10:39 -0800

    preprocessor project

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - entry point of the program
+*
+* Return: always return 0
+*/
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,1 @@
+readme file describing preprocessor projects in this directory
