cad28f0f793db655243617c6e47ded6a3a45cf63
Aanuoluwa101 <aanuayodeji101@gmail.com>
2022-11-25 07:56:29 -0800

    singly linked list task 2

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,47 @@
+#include "lists.h"
+
+/**
+* add_node - a function that adds a new node
+* at the beginning of a list_t list.
+* @head: a reference (pointer to pointer) to the head of a list
+* @str: the string to be copied into the string element of the node
+*
+* Return: the address of the new element, or NULL if it failed
+*/
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node = malloc(sizeof(list_t));
+
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		new_node->len = _strlen(new_node->str);
+		new_node->next = *head;
+		*head = new_node;
+	}
+	return (*head);
+}
+
+
+/**
+ * _strlen - a function that finds the length of a string
+ * @s: the string
+ *
+ * Return: the number of characters in a string
+ * excluding null bytes
+ */
+
+int _strlen(char *s)
+{
+	int len;
+
+	len = 0;
+	while (s[len])
+		len++;
+	return (len);
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,6 +3,7 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
 * struct list_s - singly linked list
@@ -25,4 +26,5 @@ size_t print_list(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+int _strlen(char *s);
 #endif
