9c0d2e0923225c46672766075866210d2efb90f8
Aanuoluwa101 <aanuayodeji101@gmail.com>
2022-11-25 13:43:29 -0800

    undoing last commit

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -17,43 +17,13 @@ size_t print_list(const list_t *h)
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-		{
 			printf("[0] (nil)\n");
-		}
 		else
-		{
-			if (h->len)
-			{
-				printf("[%d] %s\n", h->len, h->str);
-			}
-			else
-			{
-				h->len = _strlen(h->str);
-				printf("[%d] %s\n", h->len, h->str);
-			}
-		}
+			printf("[%d] %s\n", h->len, h->str);
 		n++;
 		h = h->next;
 	}
-
 	return (n);
 }
 
 
-/**
- * _strlen - a function that finds the length of a string
- * @s: the string
- *
- * Return: the number of characters in a string
- * excluding null bytes
- */
-
-int _strlen(char *s)
-{
-	int len;
-
-	len = 0;
-	while (s[len])
-		len++;
-	return (len);
-}
