7a0c42c83e159148049a2252fa44e11a045b54ea
Aanuoluwa101 <aanuayodeji101@gmail.com>
2022-11-25 13:35:23 -0800

    singly linked list task 0 another corerection

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -17,12 +17,43 @@ size_t print_list(const list_t *h)
 	while (h != NULL)
 	{
 		if (h->str == NULL)
+		{
 			printf("[0] (nil)\n");
+		}
 		else
-			printf("[%d] %s\n", h->len, h->str);
+		{
+			if (h->len)
+			{
+				printf("[%d] %s\n", h->len, h->str);
+			}
+			else
+			{
+				h->len = _strlen(h->str);
+				printf("[%d] %s\n", h->len, h->str);
+			}
+		}
 		n++;
 		h = h->next;
 	}
 
 	return (n);
+}
+
+
+/**
+ * _strlen - a function that finds the length of a string
+ * @s: the string
+ *
+ * Return: the number of characters in a string
+ * excluding null bytes
+ */
+
+int _strlen(char *s)
+{
+	int len;
+
+	len = 0;
+	while (s[len])
+		len++;
+	return (len);
 }
